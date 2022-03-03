cf04e14614d74c79d37e1d6f4d47a25dc62f9cc9
samgoldst <holderplace766@gmail.com>
2022-03-02 18:04:34 -0800

    Init

---
+++ src/Helper.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+
+public class Helper {
+    private static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    public static int find_index(char c){
+        for(int i = 0; i< alphabet.length; i++) {
+            if (alphabet[i] == c) {
+                return(i);
+            }
+        }
+        return 0;
+    }
+
+    public static char find_letter(int n){
+        return alphabet[n];
+    }
+}
